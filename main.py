d61de181ef61b5c351b097baa2dce9845043be65
Temiloluwa Awoyele <awoyeletemiloluwa@gmail.com>
2022-10-22 11:31:07 +0100

    Update

--- main.py
+++ main.py
@@ -5,8 +5,9 @@ from typing import Generator, Union
 
 logging.basicConfig(level=logging.INFO)
 
-data_dir:Path = Path("./data")
-json_files:Generator[Path, None, None] = data_dir.glob("*.json")
+data_dir: Path = Path("./data")
+json_files: Generator[Path, None, None] = data_dir.glob("*.json")
+
 
 def get_type(attribute: Union[str, int, dict, list]):
     if isinstance(attribute, str):
@@ -22,12 +23,27 @@ def get_type(attribute: Union[str, int, dict, list]):
     else:
         raise AttributeError(f"Attribute type not considered: {type(attribute)}")
 
+
 def crawl_schema(message: dict, update_schema: dict):
+    """
+    Crawl a dict or a mapping and infer the types, then update the details to another dict.
+
+    message: dict
+        This is the mapping which the data types of it's values is to be infered.
+
+    update_schema: dict
+        This is a mapping with keys mapped to their data types and other properties.
+    """
+    prev_key = None
     for key, value in message.items():
 
         if isinstance(value, dict):
-            logging.info("Mapping found for key: %s...Recursing..." %key)
+            logging.info(
+                "Mapping found for key: %s...Recursing...Previous key: %s"
+                % (key, prev_key)
+            )
             crawl_schema(message[key], update_schema)
+            prev_key = key
             continue
 
         key_schema = {
@@ -40,18 +56,20 @@ def crawl_schema(message: dict, update_schema: dict):
             logging.warning(f"\tkey: {key} found in update schema")
         update_schema[key] = key_schema
 
+
 if __name__ == "__main__":
+
     for i, file_path in enumerate(json_files, start=1):
+
         with open(file_path, mode="r") as json_fp:
             logging.info(f"Reading file: {file_path.name}")
             data = json.load(json_fp)
-        
-        message = data["message"]
-        message_schema = {}
-        crawl_schema(message, message_schema)
-        # logging.info(f"Schema 1: { message_schema.keys()}")
 
-        schema_write_path = f"./schema/schema_{i}_test.json"
+        message: dict = data["message"]
+        message_schema: dict = {}
+        crawl_schema(message, message_schema)
+
+        schema_write_path = f"./schema/schema_{i}.json"
         with open(schema_write_path, mode="w") as f:
             json.dump(message_schema, f, indent=4)
             logging.info(f"Schema {i} saved: {schema_write_path}")
